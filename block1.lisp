447e962b30817fe2fba6079332b2ae44e45e5ef8
KOCTYN <55509699+KOCTYN@users.noreply.github.com>
2020-03-25 13:43:56 +0300

    Update block1.lisp

--- block1.lisp
+++ block1.lisp
@@ -80,11 +80,12 @@
 ;№15
 ;Определите функцию, вычисляющую скалярное произведение векторов, заданных списками целых чисел.
 (defun cross-prod(v1 v2)
-@@ -7,7 +86,19 @@
+      (cond
+          ((null v1) 0)
+          (t (+ (* (car v1) (car v2)) (cross-prod(cdr v1) (cdr v2))))
        )
 )
-(print(cross-prod '(4 2 3) '(7 3 7)))
-;55
+(print(cross-prod '(2 3 5) '(3 7 9)))
 
 ;№18
 ;Определите предикат, проверяющий, является ли аргумент одноуровневым списком.
@@ -101,46 +102,34 @@
 
 ;№21 
 ;Определите функцию, удаляющую из списка первое вхождение данного элемента на верхнем уровне.
-@@ -18,31 +109,36 @@
+(defun del-elem(lst)
+      (cond
+         ((null lst)  nil)
+          (t(cdr lst))
        )
 )
-(print(del-elem '(2 8 5 3)))
-;(8 5 3) 
-
+(print(del-elem '(2 8 5 (3))))
 
 ;№22
 ;Определите функцию, которая обращает список (а b с) и разбивает его на уровни (((с) b) а).
 (defun level-list(lst)
       (cond
          ((null(cdr  lst)) (cons (car lst) ()))
-         ((null (cdr lst)) lst)
           ((list (level-list (cdr lst)) (car lst) ))
        )
 )
-(print(level-list '( 1 2  3 4 5 6  7 8  9 )))
-;(((((((((9) 8) 7) 6) 5) 4) 3) 2) 1) 
+(print(level-list '( 1 2  3 4 5 6  7 8  9 )))                
+(print(level-list '( 1  2 )))
 
-;№18
-;Определите предикат, проверяющий, является ли аргумент одноуровневым списком.
-(defun check-list(lst)
 ;№33
 ;Определите функцию МНОЖЕСТВО, преобразующую список в множество.
 (defun list-line(lst)
       (cond
-         ((null lst) "список одноуровневый")
-          ((atom (car lst))(check-list(cdr lst)))
-          ("список не одноуровневый")
          ((null lst) nil)
           ((listp (car lst))(append (list-line(car lst)) (list-line(cdr lst)) ))
           (t(cons (car lst) (list-line(cdr lst))))
        )
 )
-(print(check-list '((2))))
-(print(check-list '(2 (8) ((7)))))
-(print(check-list '(2 8 7)))
-;"список не одноуровневый" 
-;"список не одноуровневый" 
-;"список одноуровневый" 
 
 (defun list-set(lst)
       (cond
@@ -150,5 +139,6 @@
        )
 )
 
-(print(list-set (list-line '( 1 2  3 4 5 6  7 8  9 8 7 6 5 4 3 2 1))))                
+(print(list-set (list-line '( 1 2  3 4 5 6  7 8  9 8 7 6 5 4 3 2 1))))
 (print(list-set (list-line '((3) 2 (8) ((7)) (3) 8))))
+                
