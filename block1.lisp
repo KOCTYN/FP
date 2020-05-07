f67d9a6da43662de13c5d49b6fe0c0c77ee203a6
KOCTYN <55509699+KOCTYN@users.noreply.github.com>
2020-05-07 09:59:01 +0300

    Update block1.lisp

--- block1.lisp
+++ block1.lisp
@@ -210,3 +210,94 @@
 (print(check-list '((2))))
 (print(check-list '(2 (8) ((7)))))
 (print(check-list '(2 8 7)))
+
+
+;№33
+;Определите функцию МНОЖЕСТВО, преобразующую список в множество.
+;функция объединение списков, аналог append
+(defun app(lst1 lst2)
+      (cond
+         ((null lst1) lst2)
+         (t(cons (car lst1) (app (cdr lst1) lst2)))
+       )
+)
+
+;функция убирает вложеные списки (((a)) (b) c)->(a b c)
+(defun list-line(lst)     
+    (cond
+      ((null lst) nil)
+      (t
+        ((lambda (x y)
+          ((lambda (z)
+            (cond
+              ((null lst) nil)
+              ((listp x)(app (list-line x) z ))
+              (t(cons x z))
+             )
+            )(list-line y))
+         )(car lst)(cdr lst)
+        )
+       )
+     )
+)
+
+;функция убирает повторяющиеся элементы
+(defun list-set(lst)
+    (cond
+      ((null lst) nil)
+      (t
+        ((lambda (x y)
+          ((lambda (z)
+            (cond
+              ((null lst) nil)
+              ((member x y)z)
+              (t(cons x z))
+            )
+          )(list-set y))
+        )(car lst)(cdr lst)) 
+      )
+     )
+)
+
+(print(list-set (list-line '( 1 2  3 4 5 6  7 8  9 8 7 6 5 4 3 2 1))))
+(print(list-set (list-line '((3) 2 (8) ((7)) (3) 8))))
+
+
+;№17
+;Создайте предикат, порождающий всевозможные перестановки исходного множества.
+
+
+(defun insert-elem-in-all-position (a l r)
+  (cond 
+      ((null r) (list (append l (list a))))
+      (t
+          (cons 
+               (append l (list a) r) 
+               (insert-elem-in-all-position a (append l (list (car r))) (cdr r))
+           )
+       )
+   )
+)
+ 
+
+(defun add-elem-for-each-permutation (elem perm-lst)
+	(cond
+		((null perm-lst) nil)
+		(t (append
+				(insert-elem-in-all-position elem nil (car perm-lst))
+				(add-elem-for-each-permutation elem (cdr perm-lst))))
+	)
+)
+
+(defun my-permutation (lst)
+	(cond
+		((null lst) nil)
+		((null (cdr lst)) (list lst))
+		(t (add-elem-for-each-permutation
+			(car lst)
+			(my-permutation (cdr lst))))
+	)
+)
+
+(print (my-permutation '(1 2 3)))
+
